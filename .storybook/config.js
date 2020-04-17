e7ce732e396f84e2b22df74aa85b14acdaabc94a
Zoleyda Zambrano <zoleyda.zambrano@globant.com>
2020-04-17 01:02:26 -0300

    resolviendo issue

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from '@storybook/react';
 import '../src/index.css';
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
+registerRequireContextHook();
 
-const req = require.context('../src', true, /\.stories.js$/);
+const req = global.__requireContext(__dirname, '../src/components', true, /.stories.js$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
